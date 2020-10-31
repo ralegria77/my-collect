d8a960dc47d34478364ff2de02dc55213ba6ee4a
rebec <ralegria77@gmail.com>
2020-10-31 14:03:22 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(empty_array)
+    array_one = []
+    i = 0
+    while i < empty_array.length
+        temporary = yield(empty_array[i])
+        array_one << temporary
+        i += 1 
+    end
+array_one 
+end
 
 
